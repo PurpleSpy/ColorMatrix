759d809ccfb719bbd5be7e96e49f710a1f767aa7
PurpleSpy <36999286+PurpleSpy@users.noreply.github.com>
2024-07-18 16:42:33 -0700

    Add files via upload

---
+++ Color Marix reader/Form1.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.picturebox2 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SaveMatrix = New System.Windows.Forms.Button()
+        Me.LoadMatrix = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenMatrixToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveColorizedImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.resetMatrix = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.cameralabel1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picturebox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 31)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(426, 237)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'picturebox2
+        '
+        Me.picturebox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.picturebox2.Location = New System.Drawing.Point(444, 31)
+        Me.picturebox2.Name = "picturebox2"
+        Me.picturebox2.Size = New System.Drawing.Size(471, 235)
+        Me.picturebox2.TabIndex = 1
+        Me.picturebox2.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Location = New System.Drawing.Point(12, 323)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(903, 110)
+        Me.Panel1.TabIndex = 2
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(-12, 13)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(213, 28)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'SaveMatrix
+        '
+        Me.SaveMatrix.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.SaveMatrix.Location = New System.Drawing.Point(431, 12)
+        Me.SaveMatrix.Name = "SaveMatrix"
+        Me.SaveMatrix.Size = New System.Drawing.Size(94, 29)
+        Me.SaveMatrix.TabIndex = 4
+        Me.SaveMatrix.Text = "Save"
+        Me.SaveMatrix.UseVisualStyleBackColor = True
+        '
+        'LoadMatrix
+        '
+        Me.LoadMatrix.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.LoadMatrix.Location = New System.Drawing.Point(331, 12)
+        Me.LoadMatrix.Name = "LoadMatrix"
+        Me.LoadMatrix.Size = New System.Drawing.Size(94, 29)
+        Me.LoadMatrix.TabIndex = 5
+        Me.LoadMatrix.Text = "Load"
+        Me.LoadMatrix.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(927, 28)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenImageToolStripMenuItem, Me.OpenMatrixToolStripMenuItem, Me.CloseImageToolStripMenuItem, Me.ExitToolStripMenuItem, Me.SaveColorizedImageToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "F&ile"
+        '
+        'OpenImageToolStripMenuItem
+        '
+        Me.OpenImageToolStripMenuItem.Name = "OpenImageToolStripMenuItem"
+        Me.OpenImageToolStripMenuItem.Size = New System.Drawing.Size(237, 26)
+        Me.OpenImageToolStripMenuItem.Text = "Open Image"
+        '
+        'OpenMatrixToolStripMenuItem
+        '
+        Me.OpenMatrixToolStripMenuItem.Name = "OpenMatrixToolStripMenuItem"
+        Me.OpenMatrixToolStripMenuItem.Size = New System.Drawing.Size(237, 26)
+        Me.OpenMatrixToolStripMenuItem.Text = "Open Matrix"
+        '
+        'CloseImageToolStripMenuItem
+        '
+        Me.CloseImageToolStripMenuItem.Enabled = False
+        Me.CloseImageToolStripMenuItem.Name = "CloseImageToolStripMenuItem"
+        Me.CloseImageToolStripMenuItem.Size = New System.Drawing.Size(237, 26)
+        Me.CloseImageToolStripMenuItem.Text = "Close Image"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(237, 26)
+        Me.ExitToolStripMenuItem.Text = "Ex&it"
+        '
+        'SaveColorizedImageToolStripMenuItem
+        '
+        Me.SaveColorizedImageToolStripMenuItem.Name = "SaveColorizedImageToolStripMenuItem"
+        Me.SaveColorizedImageToolStripMenuItem.Size = New System.Drawing.Size(237, 26)
+        Me.SaveColorizedImageToolStripMenuItem.Text = "S&ave Colorized Image"
+        '
+        'resetMatrix
+        '
+        Me.resetMatrix.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.resetMatrix.Location = New System.Drawing.Point(231, 12)
+        Me.resetMatrix.Name = "resetMatrix"
+        Me.resetMatrix.Size = New System.Drawing.Size(94, 29)
+        Me.resetMatrix.TabIndex = 4
+        Me.resetMatrix.Text = "Reset"
+        Me.resetMatrix.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Controls.Add(Me.cameralabel1)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.ComboBox1)
+        Me.Panel2.Controls.Add(Me.SaveMatrix)
+        Me.Panel2.Controls.Add(Me.LoadMatrix)
+        Me.Panel2.Controls.Add(Me.resetMatrix)
+        Me.Panel2.Location = New System.Drawing.Point(12, 272)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(903, 44)
+        Me.Panel2.TabIndex = 7
+        '
+        'cameralabel1
+        '
+        Me.cameralabel1.AutoSize = True
+        Me.cameralabel1.Location = New System.Drawing.Point(867, 16)
+        Me.cameralabel1.Name = "cameralabel1"
+        Me.cameralabel1.Size = New System.Drawing.Size(0, 20)
+        Me.cameralabel1.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(716, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(145, 29)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save Camera Roll"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(540, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(170, 29)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "New Camera Roll"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(927, 453)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.picturebox2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picturebox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents picturebox2 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents SaveMatrix As Button
+    Friend WithEvents LoadMatrix As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenImageToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenMatrixToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseImageToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents resetMatrix As Button
+    Friend WithEvents SaveColorizedImageToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents cameralabel1 As Label
+End Class
---
+++ Color Marix reader/Form1.vb
@@ -0,0 +1,370 @@
+﻿Public Class Form1
+    Dim pim As Drawing.Image = Nothing
+    Dim pim2 As Drawing.Image = Nothing
+    Dim cm As New Drawing.Imaging.ColorMatrix
+    Dim loaded As Boolean = False
+    Dim backimg As New Bitmap(40, 40)
+    Dim csaves As New ArrayList
+    Dim tempzipaddress As String = ""
+    Dim crollzip As IO.Compression.ZipArchive = Nothing
+    Dim zipsream As IO.Stream = Nothing
+    Dim crollnum As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click
+        Select Case sender.text.tolower
+            Case "new camera roll"
+                sender.text = "Add Image"
+                Try
+
+
+                    If crollzip IsNot Nothing Then
+                        crollnum = 0
+                        zipsream.Close()
+                        crollzip.Dispose()
+                        IO.File.Delete(tempzipaddress)
+                        cameralabel1.Text = crollnum
+                        Button2.Enabled = False
+                    End If
+                Catch ex As Exception
+
+                End Try
+
+                tempzipaddress = My.Computer.FileSystem.GetTempFileName
+                zipsream = IO.File.Open(tempzipaddress, IO.FileMode.OpenOrCreate)
+                crollzip = New IO.Compression.ZipArchive(zipsream, IO.Compression.ZipArchiveMode.Create)
+                crollnum += 1
+                cameralabel1.Text = crollnum
+                writeEntryInZip(crollzip, pim, "base.png")
+            Case "add image"
+                crollnum += 1
+                Button2.Enabled = True
+                cameralabel1.Text = crollnum
+                writeEntryInZip(crollzip, pim2, "colorized" & crollnum & ".png")
+            Case "save camera roll"
+                Button1.Text = "New Camera Roll"
+                Try
+                    Using cp As New SaveFileDialog
+                        cp.Filter = "camera roll zip file|*.zip"
+                        zipsream.Flush()
+                        crollzip.Dispose()
+
+                        If cp.ShowDialog = DialogResult.OK Then
+
+                            IO.File.Move(tempzipaddress, cp.FileName)
+                        End If
+                        Button2.Enabled = False
+                    End Using
+                Catch ex As Exception
+
+                End Try
+
+        End Select
+
+
+    End Sub
+
+    Public Sub writeEntryInZip(zip As IO.Compression.ZipArchive, img As Drawing.Image, fname As String)
+        Dim ne As IO.Compression.ZipArchiveEntry = zip.CreateEntry(fname)
+
+        Dim encoderingo As Imaging.ImageCodecInfo = grabencoder("png")
+        Dim enc As New Imaging.Encoder(encoderingo.Clsid)
+        Dim encoderparam As New Imaging.EncoderParameters(1)
+        Dim qualparm As New Imaging.EncoderParameter(enc, 5L)
+        encoderparam.Param(0) = qualparm
+        Dim be As IO.Stream = ne.Open
+        img.Save(be, encoderingo, encoderparam)
+        be.Flush()
+        be.Close()
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim icount As Integer = 0
+        Dim y = 0
+        For Each itm As Reflection.PropertyInfo In cm.GetType.GetProperties
+            If icount > 1 And icount Mod 5 = 0 Then
+
+                y += 1
+
+            End If
+            If itm.Name.ToLower.IndexOf("matrix") = 0 Then
+
+                Dim cnum As New NumericUpDown
+
+                cnum.DecimalPlaces = 3
+                cnum.Increment = 0.01
+                cnum.Minimum = -5
+                cnum.Maximum = 5
+
+
+
+                cnum.Top = y * cnum.Height
+                cnum.Left = (icount Mod 5) * cnum.Width
+                cnum.Name = itm.Name
+                cnum.Value = itm.GetValue(cm)
+                Panel1.HorizontalScroll.Enabled = True
+                Panel1.VerticalScroll.Enabled = True
+
+                Panel1.Controls.Add(cnum)
+                AddHandler cnum.ValueChanged, AddressOf matrixchange
+                icount += 1
+            End If
+        Next
+
+        Using drg As Drawing.Graphics = Drawing.Graphics.FromImage(backimg)
+            Dim ccolor As Drawing.Color = Drawing.Color.Azure
+            Dim bbrush As New Drawing.SolidBrush(ccolor)
+
+            drg.FillRectangle(bbrush, New Rectangle(0, 0, 20, 20))
+            drg.FillRectangle(bbrush, New Rectangle(20, 20, 20, 20))
+        End Using
+
+        PictureBox1.BackgroundImage = backimg
+        PictureBox1.BackgroundImageLayout = ImageLayout.Tile
+        picturebox2.BackgroundImage = backimg
+        picturebox2.BackgroundImageLayout = ImageLayout.Tile
+        PictureBox1.Cursor = Cursors.Hand
+        picturebox2.Cursor = Cursors.Hand
+
+        csaves.Add(New ColorMatrixSave)
+        csaves(0).name = "Default"
+
+        ComboBox1.Items.Add(csaves(0).name)
+        Panel1.Enabled = False
+        Panel2.Enabled = False
+    End Sub
+
+    Public Sub matrixchange(sender As Object, e As EventArgs)
+        updateImages()
+    End Sub
+
+
+    Function createMatrix() As Drawing.Imaging.ColorMatrix
+        Dim cmat As New Drawing.Imaging.ColorMatrix
+        For Each itm As NumericUpDown In Panel1.Controls
+            If itm.Name.ToLower.IndexOf("matrix") = 0 Then
+                cmat.GetType.GetProperty(itm.Name).SetValue(cmat, Single.Parse(itm.Value))
+
+            End If
+
+        Next
+        Return cmat
+    End Function
+
+    Public Function appyMatrix(img As Drawing.Image, matrix As Imaging.ColorMatrix) As Image
+        Dim sp As New Bitmap(img.Width, img.Height)
+        Dim xrc As New Imaging.ImageAttributes
+
+        xrc.SetColorMatrix(matrix)
+        sp.MakeTransparent()
+
+        Using gdi As Drawing.Graphics = Drawing.Graphics.FromImage(sp)
+
+            gdi.DrawImage(img, New Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, xrc)
+        End Using
+
+        Return sp
+    End Function
+    Private Sub OpenImageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenImageToolStripMenuItem.Click
+        Try
+            Using srp As New OpenFileDialog
+                srp.Filter = "Images|*.*"
+                srp.FilterIndex = 1
+
+                If srp.ShowDialog = DialogResult.OK Then
+                    Try
+                        Dim bim As Image = Image.FromFile(srp.FileName)
+                        cm = createMatrix()
+                        pim = bim
+                        updateImages()
+                        Panel1.Enabled = True
+                        Panel2.Enabled = True
+                        CloseImageToolStripMenuItem.Enabled = True
+                    Catch ex As Exception
+                        MessageBox.Show("File not accepted")
+                    End Try
+                End If
+            End Using
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Sub updateImages()
+        Try
+            cm = createMatrix()
+            pim2 = appyMatrix(pim, cm)
+            PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+            picturebox2.SizeMode = PictureBoxSizeMode.Zoom
+            PictureBox1.Image = pim
+            picturebox2.Image = pim2
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub SaveColorizedImageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveColorizedImageToolStripMenuItem.Click
+        Using dc As New SaveFileDialog
+            dc.DefaultExt = "png"
+            dc.Filter = "Png file|*.png"
+            dc.FilterIndex = 1
+
+            Try
+                If dc.ShowDialog = DialogResult.OK Then
+                    Try
+                        Using ffname As IO.FileStream = IO.File.Open(dc.FileName, IO.FileMode.Create)
+                            Dim encoderingo As Imaging.ImageCodecInfo = grabencoder("png")
+                            Dim enc As New Imaging.Encoder(encoderingo.Clsid)
+                            Dim encoderparam As New Imaging.EncoderParameters(1)
+                            Dim qualparm As New Imaging.EncoderParameter(enc, 5L)
+                            encoderparam.Param(0) = qualparm
+                            pim2.Save(ffname, encoderingo, encoderparam)
+                        End Using
+                    Catch ex As Exception
+
+                    End Try
+                End If
+            Catch ex As Exception
+
+            End Try
+
+        End Using
+    End Sub
+
+    Public Function grabencoder(mime As String) As Imaging.ImageCodecInfo
+        For Each itm As Imaging.ImageCodecInfo In Imaging.ImageCodecInfo.GetImageEncoders
+            If itm.MimeType.ToLower.IndexOf(mime.ToLower) > 1 Then
+                Return itm
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    Public Sub viewImage(img As Image, backimg As Image)
+        Dim fform As New Form
+        Dim cpan As New Panel
+        Dim pbox As New PictureBox
+
+        cpan.Controls.Add(pbox)
+        fform.Controls.Add(cpan)
+        cpan.Dock = DockStyle.Fill
+        cpan.AutoScroll = True
+        cpan.BackgroundImage = backimg
+        cpan.BackgroundImageLayout = ImageLayout.Tile
+        pbox.SizeMode = PictureBoxSizeMode.AutoSize
+
+        pbox.Image = img
+
+        pbox.Location = New Point(0, 0)
+        cpan.Location = New Point(0, 0)
+
+        fform.Show()
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click, picturebox2.Click
+        Select Case sender.name.tolower
+            Case "picturebox1"
+                If pim IsNot Nothing Then
+                    viewImage(pim, backimg)
+                End If
+
+            Case "picturebox2"
+                If pim IsNot Nothing Then
+                    viewImage(pim2, backimg)
+                End If
+        End Select
+
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub CloseImageToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseImageToolStripMenuItem.Click
+        pim2 = Nothing
+        pim = Nothing
+        sender.enabled = False
+        Panel1.Enabled = False
+        Panel2.Enabled = False
+        PictureBox1.Image = Nothing
+        picturebox2.Image = Nothing
+        crollnum = 0
+        Button1.Text = "New Camera Roll"
+    End Sub
+
+
+End Class
+
+Public Class ColorMatrixSave
+    Public Property ColorMatrix As New Drawing.Imaging.ColorMatrix
+    Public Property Name As String = ""
+    Public Property MatrixDetails As New Collections.SortedList
+    Sub New()
+
+    End Sub
+
+    Public Function createMatrix() As Drawing.Imaging.ColorMatrix
+        ColorMatrix = New Drawing.Imaging.ColorMatrix
+        For Each itm As Reflection.PropertyInfo In MatrixDetails.GetType.GetProperties
+            If MatrixDetails.ContainsKey(itm.Name) Then
+                ColorMatrix.GetType.GetProperty(itm.Name).SetValue(ColorMatrix, MatrixDetails(itm.Name))
+            End If
+        Next
+
+        Return ColorMatrix
+    End Function
+
+    Public Sub saveMatrix(path As String)
+        MatrixDetails.Clear()
+        MatrixDetails("Name") = Name
+        For Each itm As Reflection.PropertyInfo In ColorMatrix.GetType.GetProperties
+
+            If itm.Name.ToLower.IndexOf("matrix") = 0 Then
+                MatrixDetails(itm.Name) = ColorMatrix.GetType.GetProperty(itm.Name).GetValue(ColorMatrix)
+            End If
+        Next
+
+
+
+        Dim jeer As New System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
+        Using xmr As IO.Stream = IO.File.OpenWrite(path)
+            Using strre As New IO.StreamWriter(xmr)
+                Dim jsout As String = System.Text.Json.JsonSerializer.Serialize(MatrixDetails)
+                strre.Write(jsout)
+                strre.Flush()
+                strre.Close()
+            End Using
+
+
+        End Using
+
+
+    End Sub
+
+    Public Sub loadMatrix(path As String)
+        If IO.File.Exists(path) Then
+            Try
+
+                Using stre As IO.Stream = IO.File.Open(path, IO.FileMode.Open)
+
+                    Dim testval As New SortedList
+                    testval = System.Text.Json.JsonSerializer.Deserialize(stre, testval.GetType)
+                    If testval.ContainsKey("Name") Then
+                        Name = testval(Name)
+                    End If
+
+                    MatrixDetails = testval
+                    stre.Close()
+                    Me.ColorMatrix = createMatrix()
+                End Using
+            Catch ex As Exception
+
+            End Try
+
+
+
+
+        End If
+    End Sub
+
+End Class
